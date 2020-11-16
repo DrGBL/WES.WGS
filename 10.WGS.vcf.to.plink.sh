973e58fdf8751df0f9bee02fd6cb416303033015
DrGBL <guillaume_bl@hotmail.com>
2020-11-16 15:35:38 -0500

    Create 10.WGS.vcf.to.plink.sh

---
+++ 10.WGS.vcf.to.plink.sh
@@ -0,0 +1,15 @@
+#this takes the filtered vcf file from the WGS sequencing, removes variants with MAF < 1%, MAC < 5, and for whom HWE is not reached, and outputs the plink binary file.
+#genotype call rate and MAF threshold is set high to avoid quasi-separation, since step 1 does not use Firth
+#this file is to be used in step 1 of regenie. regenie does not accept vcf as inputs.
+#warning: may take a bit of time.
+
+mkdir -p regenieInputs
+
+plink --vcf /path/to/sequence.file.Eur.normID.rehead.GTflt.AB.noChrXYM.vcf.gz \
+ --hwe 1E-6 midp \
+ --make-bed \
+ --maf 0.05 \
+ --mac 5 \
+ --geno 0.95 \
+ --out /regenieInputs/sequence.file.Eur.normID.rehead.GTflt.AB.noChrXYM 
+ 
