7fa1eebc30b328624a4af519a921fdc75bda1a90
DrGBL <guillaume_bl@hotmail.com>
2020-11-16 16:03:48 -0500

    Update 11.makeRegenieVCF.sh

--- 11.makeRegenieVCF.sh
+++ 11.makeRegenieVCF.sh
@@ -2,37 +2,43 @@
 #it uses the M1.GT.R M2.GT.R M3.GT.R and M4.GT.R functions
 
 #builds the header for the vcf file I'm building
+printf "##fileformat=VCFv4.2\n##fileDate=20201110\n##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">" > headerStep1Final.txt
 zcat /scratch/richards/guillaume.butler-laporte/WGS/allSamples.Eur.normID.rehead.GTflt.AB.noChrXYM.vqsr.flt.vcf.gz | head -n 5000 | grep '#CHROM'  > /project/richards/guillaume.butler-laporte/WGS/bqc.individual.wgs.20200908/VCF.sct.regenie/columns.txt
 
 ###### M1 note that this uses max AF among 1000 genomes, ESP, and gnomAD populations, combined. ######
+mkdir -p M1
+mkdir -p M1/finalMask
 #first filter for the correct variants
-for i in {1..22}; do awk '/LoF=HC/ && /CANONICAL=YES/ && !/MAX_AF=0.[0-9][1-9]*;/' /scratch/richards/guillaume.butler-laporte/WGS/annotation/finalAnnot.chr${i}.txt > /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/M1.annot.chr${i}.txt; done
+for i in {1..22}; do awk '/LoF=HC/ && /CANONICAL=YES/ && !/MAX_AF=0.[0-9][1-9]*;/' /path/to/annotation/finalAnnot.chr${i}.txt > /M1/M1.annot.chr${i}.txt; done
 
 #now obtain files listing the variants and their corresponding genes, ordered by chromosomal position
-for i in {1..22}; do awk '{ print $4, $1 }' /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/M1.annot.chr${i}.txt | sort -k 2 | uniq > /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/M1.long.chr${i}.txt; done
-for i in {1..22}; do awk '{ print $2 }' /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/M1.long.chr${i}.txt > /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/M1.variants.chr${i}.txt; done
+for i in {1..22}; do awk '{ print $4, $1 }' /M1/M1.annot.chr${i}.txt | sort -k 2 | uniq > /M1/M1.long.chr${i}.txt; done
+for i in {1..22}; do awk '{ print $2 }' /M1/M1.long.chr${i}.txt > /M1/M1.variants.chr${i}.txt; done
 
 #now use bcftools to view only those variants and obtain each sample's genotype
-for i in {1..22}; do bcftools view -i "%ID=@/scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/M1.variants.chr${i}.txt" /scratch/richards/guillaume.butler-laporte/WGS/splitChrom/allSamples.chr${i}.Eur.normID.rehead.GTflt.AB.noChrXYM.vqsr.flt.vcf.gz -Ou | bcftools filter -i 'INFO/MAF<=0.01' -Ou | bcftools query -f '%ID [ %GT] \n' > /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/M1.GT.chr${i}.txt; done
+for i in {1..22}; do bcftools view -i "%ID=@/M1/M1.variants.chr${i}.txt" /path/to/sequence.file.chr${i}.Eur.normID.rehead.GTflt.AB.noChrXYM.vcf.gz -Ou | bcftools filter -i 'INFO/MAF<=0.01' -Ou | bcftools query -f '%ID [ %GT] \n' > /M1/M1.GT.chr${i}.txt; done
 
 #now use R to build the genotype
 Rscript /project/richards/guillaume.butler-laporte/WGS/bqc.individual.wgs.20200908/finalScripts/M1.GT.R
-for i in {1..22}; do cat /project/richards/guillaume.butler-laporte/WGS/bqc.individual.wgs.20200908/VCF.sct.regenie/headerStep1.txt /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/finalMask/M1.chr${i}.txt > /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/finalMask/M1.chr${i}.vcf; done
+for i in {1..22}; do cat headerStep1.txt /M1/finalMask/M1.chr${i}.txt > /M1/finalMask/M1.chr${i}.vcf; done
 
 #now use plink to build 
-for i in {1..22}; do plink --vcf /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/finalMask/M1.chr${i}.vcf --make-bed --out /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/finalMask/M1.chr${i}; done
+for i in {1..22}; do plink --vcf /M1/finalMask/M1.chr${i}.vcf --make-bed --out /M1/finalMask/M1.chr${i}; done
 
 #get a list of those plink files
-find /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/finalMask/ -name "*.bim" | grep -e "Pruned" > /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/finalMask/ForMerge.list ;
+find /M1/finalMask/ -name "*.bim" | grep -e "Pruned" > /M1/finalMask/ForMerge.list ;
 
-sed -i 's/.bim//g' /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/finalMask/ForMerge.list ;
+sed -i 's/.bim//g' /M1/finalMask/ForMerge.list ;
 
 #merge all files in one
-plink --merge-list /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/finalMask/ForMerge.list --out /scratch/richards/guillaume.butler-laporte/WGS/Masks/M1/finalMask/MergeM1 ;
+plink --merge-list /M1/finalMask/ForMerge.list --out /M1/finalMask/MergeM1 ;
 
 
 
 ####### M2, again note the max AF issue #########
+
+mkdir -p M2
+
 #first filter for the correct variants
 for i in {1..22}; do awk '(/LoF=HC/ || /missense_variant/) && /CANONICAL=YES/ && !/MAX_AF=0.[0-9][1-9]*;/' /scratch/richards/guillaume.butler-laporte/WGS/annotation/finalAnnot.chr${i}.txt > /scratch/richards/guillaume.butler-laporte/WGS/Masks/M2/M2.annot.chr${i}.txt; done
 
@@ -61,6 +67,9 @@ plink --merge-list /scratch/richards/guillaume.butler-laporte/WGS/Masks/M2/final
 
 
 ####### M3, again note the max AF issue #########
+
+mkdir -p M3
+
 #first filter for the correct variants
 for i in {1..22}; do awk '(/loF=HC/ || (/missense_variant/ && /SIFT_pred=[,DT]*D/ && /Polyphen2_HVAR_pred=[,DPB]*D[,DPB]*;/ && /Polyphen2_HDIV_pred=[,DPB]*D[,DPB]*;/ && (/MutationTaster_pred=[,ADNP]*D[,ADNP]*;/ || /MutationTaster_pred=[,ADNP]*A[,ADNP]*;/) && /LRT_pred=[;DNU]*D[;DNU]*;/)) && /CANONICAL=YES/ && !/MAX_AF=0.[0-9][1-9]*;/' /scratch/richards/guillaume.butler-laporte/WGS/annotation/finalAnnot.chr${i}.txt > /scratch/richards/guillaume.butler-laporte/WGS/Masks/M3/M3.annot.chr${i}.txt; done
 
@@ -89,6 +98,9 @@ plink --merge-list /scratch/richards/guillaume.butler-laporte/WGS/Masks/M3/final
 
 
 ####### M4, again note the max AF issue #########
+
+mkdir -p M4
+
 #first filter for the correct variants
 for i in {1..22}; do awk '(/loF=HC/ || (/missense_variant/ && (/SIFT_pred=[,DT]*D/ || /Polyphen2_HVAR_pred=[,DPB]*D[,DPB]*;/ || /Polyphen2_HDIV_pred=[,DPB]*D[,DPB]*;/ || /MutationTaster_pred=[,ADNP]*D[,ADNP]*;/ || /MutationTaster_pred=[,ADNP]*A[,ADNP]*;/ || /LRT_pred=[;DNU]*D[;DNU]*;/))) && /CANONICAL=YES/ && !/MAX_AF=0.[0-9][1-9]*;/' /scratch/richards/guillaume.butler-laporte/WGS/annotation/finalAnnot.chr${i}.txt > /scratch/richards/guillaume.butler-laporte/WGS/Masks/M4/M4.annot.chr${i}.txt; done
 
