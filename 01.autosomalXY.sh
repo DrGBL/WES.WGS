7e12fb42dfe1d2812db04bfd1d2731672a1c0db4
DrGBL <guillaume_bl@hotmail.com>
2020-11-16 23:45:00 -0500

    Update 01.autosomalXY.sh

--- 01.autosomalXY.sh
+++ 01.autosomalXY.sh
@@ -4,5 +4,5 @@
 
 bcftools filter -r chr1,chr2,chr3,ch4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY \
   /path/to/sequencing.file.vcf.gz \
-  -Oz > /path/to/sequencing.file.noChrXYM.vcf.gz
-tabix -p vcf /path/to/sequencing.file.noChrXYM.vcf.gz
+  -Oz > /path/to/sequencing.file.noChrM.vcf.gz
+tabix -p vcf /path/to/sequencing.file.noChrM.vcf.gz
