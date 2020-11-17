04ffa9379e9cd21150cb53b7aed240bf8e881e3f
DrGBL <guillaume_bl@hotmail.com>
2020-11-16 23:39:56 -0500

    Update 02.hail.py

--- 02.hail.py
+++ 02.hail.py
@@ -8,9 +8,9 @@ import hail as hl
 hl.init(spark_conf=None, tmp_dir='/path/to/tmp_dir/')
 
 #import the data and sample QC
-hl.import_vcf('/path/to/sequence.file.noChrXYM.vcf.gz', min_partitions=4, reference_genome='GRCh38', force_bgz=True).write('/scratch/richards/guillaume.butler-laporte/WGS/hailFiles/hail.full.noChrXYM.mt', overwrite=True)
+hl.import_vcf('/path/to/sequence.file.noChrXY.vcf.gz', min_partitions=4, reference_genome='GRCh38', force_bgz=True).write('/scratch/richards/guillaume.butler-laporte/WGS/hailFiles/hail.full.noChrXY.mt', overwrite=True)
 
-mtAll = hl.read_matrix_table('/scratch/richards/guillaume.butler-laporte/WGS/hailFiles/hail.full.noChrXYM.mt')
+mtAll = hl.read_matrix_table('/scratch/richards/guillaume.butler-laporte/WGS/hailFiles/hail.full.noChrXY.mt')
 mtAll= mtAll.annotate_entries(AB = (mtAll.AD[1] / hl.sum(mtAll.AD) ))
 mtAll=hl.sample_qc(mtAll)
 mtAll = mtAll.filter_cols((mtAll.sample_qc.call_rate >= 0.97) & (mtAll.sample_qc.dp_stats.mean >= 20))
@@ -20,4 +20,4 @@ mtAll = mtAll.filter_entries( (mtAll.GQ>=20) &
                         (mtAll.GT.is_het() & (mtAll.AB >= 0.25) & (mtAll.AB <= 0.75)) |
                         (mtAll.GT.is_hom_var() & (mtAll.AB >= 0.9))))
                         
-hl.export_vcf(mtAll, '/path/to/sequence.file.GTflt.AB.noChrXYM.vcf.gz')
+hl.export_vcf(mtAll, '/path/to/sequence.file.GTflt.AB.noChrXY.vcf.gz')
