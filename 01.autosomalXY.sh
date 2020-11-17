5673e52a6a951651a94c78115d8263441017f3e5
DrGBL <guillaume_bl@hotmail.com>
2020-11-16 23:38:25 -0500

    Update and rename 01.autosomal.sh to 01.autosomalXY.sh

--- 01.autosomalXY.sh
+++ 01.autosomalXY.sh
@@ -1,8 +1,8 @@
 #The "sequencing.file.vcf.gz" file below is the variant call vcf. Note that at our center it comes with variant recalibration already performed.
 
-#This removes all non autosomal chromosomes from further analysis.
+#This removes mitochondrial DNA
 
-bcftools filter -r chr1,chr2,chr3,ch4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22 \
+bcftools filter -r chr1,chr2,chr3,ch4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY \
   /path/to/sequencing.file.vcf.gz \
   -Oz > /path/to/sequencing.file.noChrXYM.vcf.gz
 tabix -p vcf /path/to/sequencing.file.noChrXYM.vcf.gz
