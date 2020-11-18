98c9bdd6544a922ff716f8553c540c4b058f03a5
DrGBL <guillaume_bl@hotmail.com>
2020-11-18 09:29:18 -0500

    Update 03.reheader.sh

--- 03.reheader.sh
+++ 03.reheader.sh
@@ -5,7 +5,9 @@
 bcftools view -h /path/to/sequence.file.noChrM.vcf.gz > /path/to/header.txt
 
 #obtain the header from the hail output vcf
-bcftools view -h /path/to/sequence.file.GTflt.AB.noChrM.vcf.gz > /path/to/postHailHeader.txt
+#Here I used zcat and grep because bcftools misbehaved on the post-hail vcf file. I suspect it's because the header was not formatted well. Another reason to rehead it.
+#the "head" call is simply because I know that the header is the first few lines, so there's no reason for grep to go through the whole genome, which would be very slow.
+zcat /path/to/sequence.file.GTflt.AB.noChrM.vcf.gz | head -n 5000 | grep "#" > /path/to/postHailHeader.txt
 
 #the postHailHeader.txt header will need to be modified manually so that the metadata works
 #e.g. in the postHailHeader you will have: ##FORMAT=<ID=AD,Number=.,Type=Integer,Description="">
