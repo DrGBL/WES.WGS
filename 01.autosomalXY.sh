d8cdff4ae2ed6d9b196a1fd1deb690210723bf0b
DrGBL <guillaume_bl@hotmail.com>
2020-11-20 11:14:39 -0500

    Update 01.autosomalXY.sh

--- 01.autosomalXY.sh
+++ 01.autosomalXY.sh
@@ -2,7 +2,7 @@
 
 #This removes mitochondrial DNA
 
-bcftools filter -r chr1,chr2,chr3,ch4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY \
+bcftools filter -r chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY \
   /path/to/sequencing.file.vcf.gz \
   -Oz > /path/to/sequencing.file.noChrM.vcf.gz
 tabix -p vcf /path/to/sequencing.file.noChrM.vcf.gz
