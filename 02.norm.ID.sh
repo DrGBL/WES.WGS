d2cf4d51f6ad821dac0b9365f021b8597d5348f0
DrGBL <guillaume_bl@hotmail.com>
2020-11-23 10:26:10 -0500

    Update 02.norm.ID.sh

--- 02.norm.ID.sh
+++ 02.norm.ID.sh
@@ -4,8 +4,8 @@
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 gzip -d GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 
-bcftools norm -m -any --check-ref w -f /path/to/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna /path/to/sequence.file.rehead.GTflt.AB.noChrM.vcf.gz -Ou | \
-  bcftools annotate --set-id '%CHROM:%POS:%REF:%FIRST_ALT' -Oz > /path/to/sequence.file.normID.rehead.GTflt.AB.noChrM.vcf.gz
-tabix -p vcf /path/to/sequence.file.normID.rehead.GTflt.AB.noChrM.vcf.gz
+bcftools norm -m -any --check-ref w -f /path/to/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna /path/to/sequence.file.noChrM.vcf.gz -Ou | \
+  bcftools annotate --set-id '%CHROM:%POS:%REF:%FIRST_ALT' -Oz > /path/to/sequence.file.normID.noChrM.vcf.gz
+tabix -p vcf /path/to/sequence.file.normID.noChrM.vcf.gz
 
 
