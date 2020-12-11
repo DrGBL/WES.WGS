622aae6bf218f095fe38fc7628cb46e7daff4d5d
DrGBL <guillaume_bl@hotmail.com>
2020-12-11 13:56:34 -0500

    Update M2.GT.R

--- M2.GT.R
+++ M2.GT.R
@@ -1,9 +1,12 @@
 #the following 4 lines need to be adjusted to your folders
 
 pathGT<-"/path/to/M2/M2.GT.chr"
+pathGTExclusion<-"/path/to/M2.2/M2.2.GT.chr"
 pathCol<-"/path/to/columns.txt"
 pathLong<-"/path/to/M2/M2.long.chr"
 pathOut<-"/path/to/M2/finalMask/"
+pathOutExclusion<-"/path/to/M2.2/finalMask/"
+
 
 
 library(tidyverse)
@@ -23,7 +26,7 @@ mergeGT<-function(GT){
 columns<-scan(pathCol, what=character())
 
 for(i in 1:22){
- 
+  #the masks with gnomad/esp only
   M2.GT <- read.table(paste0(pathGT,i,".txt"), quote="\"", comment.char="")
   colnames(M2.GT)[2:ncol(M2.GT)]<-columns[10:length(columns)]
   colnames(M2.GT)[1]<-"variants"
@@ -55,5 +58,40 @@ for(i in 1:22){
   M2<-M2[which(rows.to.keep),]
   
   write.table(M2, paste0(pathOut,"M2.chr",i,".txt"),row.names=FALSE,sep="\t", quote = FALSE)
+
+  
+  #the masks with the merged exclusion list too
+  
+  M2.2.GT <- read.table(paste0(pathGTExclusion,i,".txt"), quote="\"", comment.char="")
+  colnames(M2.2.GT)[2:ncol(M2.2.GT)]<-columns[10:length(columns)]
+  colnames(M2.2.GT)[1]<-"variants"
+  
+  M2.2.long<-read.table(paste0(pathLong,i,".txt"), quote="\"", comment.char="")
+  colnames(M2.2.long)<-c("gene", "variants")
+  
+  #tmp M2.2 file to work on to merge genotypes
+  M2.2.tmp<-merge(M2.2.long, M2.2.GT)
+  M2.2.tmp$gene<-droplevels(M2.2.tmp$gene)
+  nVar<-which(colnames(M2.2.tmp)=="variants")
+  M2.2.tmp<-M2.2.tmp[,-nVar]
+
+  M2.2.tmp<-M2.2.tmp %>% 
+    group_by(gene) %>%
+    summarise(across(columns[10:length(columns)],mergeGT))
+  
+  colnames(M2.2.tmp)[1]<-"ID"
+  
+  #this is the vcf
+  nGene<-nrow(table(M2.2.tmp$ID))
+  M2.2<-data.frame("#CHROM"=rep(paste0("chr",i), nGene), POS=c(1:nGene), ID=names(table(M2.2.tmp$ID)), REF=rep("C",nGene), ALT=rep("A",nGene), QUAL=rep(".", nGene), FILTER=rep(".", nGene), INFO=rep(".", nGene), FORMAT=rep("GT", nGene))
+  colnames(M2.2)[1]<-"#CHROM"
+  M2.2<-merge(M2.2,M2.2.tmp)
+  M2.2<-M2.2 %>% relocate("#CHROM", "POS")
+  
+  #this removes rows that are all 0/0 or 1/1 since there are no variants here, this is an artefact of subsetting on ancestry
+  rows.to.keep<-apply(M2.2, 1, function(r) {!(all(r == "1/1") | all(r == "0/0"))})
+  M2.2<-M2.2[which(rows.to.keep),]
+  
+  write.table(M2.2, paste0(pathOutExclusion,"M2.2.chr",i,".txt"),row.names=FALSE,sep="\t", quote = FALSE)
 
 }
