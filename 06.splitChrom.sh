c7151370d99824a882a97e88d720be9786be0cfb
DrGBL <guillaume_bl@hotmail.com>
2021-02-01 10:43:35 -0500

    Update 06.splitChrom.sh

--- 06.splitChrom.sh
+++ 06.splitChrom.sh
@@ -1,8 +1,8 @@
 #some of the next few steps were too slow to do on the whole genome, and so I split my file in chromosomes.
 #this is one of the steps that should 100% be performed using PBS arrays, if available, to speed up computing time
 
-for chr in {1..22} X Y; do
-  bcftools filter -r chr"${chr}" /path/to/sequence.file.Eur.normID.rehead.GTflt.AB.noChrM.vcf.gz -Oz | \
-    > /path/to/sequence.file.chr"${chr}".Eur.normID.rehead.GTflt.AB.noChrM.vcf.gz
-    tabix -p vcf /path/to/sequence.file.chr"${chr}".Eur.normID.rehead.GTflt.AB.noChrM.vcf.gz;
+for chr in {1..22} X; do
+  bcftools filter -r chr"${chr}" /path/to/sequence.file.Eur.normID.GTflt.AB.noChrM.vcf.gz -Oz | \
+    > /path/to/sequence.file.chr"${chr}".Eur.normID.GTflt.AB.noChrM.vcf.gz
+    tabix -p vcf /path/to/sequence.file.chr"${chr}".Eur.normID.GTflt.AB.noChrM.vcf.gz;
 done
