a30e41b80cb9c03c546e54b2930bcc0acb776b2a
DrGBL <guillaume_bl@hotmail.com>
2021-01-31 21:23:27 -0500

    Delete 01.autosomalXY.sh

--- 01.autosomalXY.sh
+++
@@ -1,8 +0,0 @@
-#The "sequencing.file.vcf.gz" file below is the variant call vcf. Note that at our center it comes with variant recalibration already performed.
-
-#This removes mitochondrial DNA
-
-bcftools filter -r chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY \
-  /path/to/sequencing.file.vcf.gz \
-  -Oz > /path/to/sequencing.file.noChrM.vcf.gz
-tabix -p vcf /path/to/sequencing.file.noChrM.vcf.gz
