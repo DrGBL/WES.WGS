fb78aeb5a4d05232bb17581fd3c4a98d919f92aa
DrGBL <guillaume_bl@hotmail.com>
2021-02-01 09:48:29 -0500

    Update 04.selectAncestry.sh

--- 04.selectAncestry.sh
+++ 04.selectAncestry.sh
@@ -1,6 +1,6 @@
 #this subsets the vcf to only the ancestry of interest. I took european ancestry as an example. This will need to be changed based on what you're working on.
 
 bcftools view -S /path/to/eurIDsPCA.txt \
-  /path/to/sequence.file.normID.rehead.GTflt.AB.noChrM.vcf.gz -Oz > /path/to/sequence.file.Eur.normID.rehead.GTflt.AB.noChrM.vcf.gz
+  /path/to/sequence.file.normID.GTflt.AB.noChrM.vcf.bgz -Oz > /path/to/sequence.file.Eur.normID.GTflt.AB.noChrM.vcf.gz
   
-tabix -p vcf /path/to/sequence.file.Eur.normID.rehead.GTflt.AB.noChrM.vcf.gz
+tabix -p vcf /path/to/sequence.file.Eur.normID.GTflt.AB.noChrM.vcf.gz
