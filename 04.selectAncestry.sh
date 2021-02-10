bd5d698805477dc5cfd5f235a8d7e926000cb6cb
DrGBL <guillaume_bl@hotmail.com>
2021-02-09 23:00:24 -0500

    Update 04.selectAncestry.sh

--- 04.selectAncestry.sh
+++ 04.selectAncestry.sh
@@ -3,13 +3,13 @@
 #paths
 
 #path to ancestry IDs
-pathID = /scratch/richards/guillaume.butler-laporte/WGS/1000G.PCA/eurIDsPCA.txt
+pathID=/scratch/richards/guillaume.butler-laporte/WGS/1000G.PCA/eurIDsPCA.txt
 
 #path to QCed vcf
-pathQC = /scratch/richards/guillaume.butler-laporte/WGS/QCed/allSamples.normID.GTflt.AB.noChrM.vqsr.flt.vcf.bgz
+pathQC=/scratch/richards/guillaume.butler-laporte/WGS/QCed/allSamples.normID.GTflt.AB.noChrM.vqsr.flt.vcf.bgz
 
 #path to QCed vcf restricted to the ancestry of interest
-pathAncestry = /scratch/richards/guillaume.butler-laporte/WGS/allSamples.Eur.normID.GTflt.AB.noChrM.vqsr.flt.vcf.gz
+pathAncestry=/scratch/richards/guillaume.butler-laporte/WGS/allSamples.Eur.normID.GTflt.AB.noChrM.vqsr.flt.vcf.gz
 
 #this splits the vcf to only the ancestry of interest. To change based on what you're working on.
 
