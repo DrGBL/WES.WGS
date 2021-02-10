d1b365092eed487cac68820d484db6257b120b8e
DrGBL <guillaume_bl@hotmail.com>
2021-02-09 23:02:34 -0500

    Update 06.splitChrom.sh

--- 06.splitChrom.sh
+++ 06.splitChrom.sh
@@ -1,8 +1,13 @@
 #some of the next few steps were too slow to do on the whole genome, and so I split my file in chromosomes.
 #this is one of the steps that should 100% be performed using PBS arrays, if available, to speed up computing time
 
+#path to QCed vcf restricted to given ancestry
+pathAncestry=/scratch/richards/guillaume.butler-laporte/WGS/allSamples.Eur.normID.GTflt.AB.noChrM.vqsr.flt.vcf.gz
+
+#path to split chromosomes output
+pathSplit=/scratch/richards/guillaume.butler-laporte/WGS/splitChrom/allSamples.chr${x}.Eur.normID.GTflt.AB.noChrM.vqsr.flt.vcf.gz
+
 for chr in {1..22} X; do
-  bcftools filter -r chr"${chr}" /path/to/sequence.file.Eur.normID.GTflt.AB.noChrM.vcf.gz -Oz | \
-    > /path/to/sequence.file.chr"${chr}".Eur.normID.GTflt.AB.noChrM.vcf.gz
-    tabix -p vcf /path/to/sequence.file.chr"${chr}".Eur.normID.GTflt.AB.noChrM.vcf.gz;
+  bcftools filter -r chr${x}  $pathAncestry -Oz > ${pathSplit}
+  tabix -p vcf ${pathSplit};
 done
