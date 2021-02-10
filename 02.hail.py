cd9515b639e2aceaeffb63359ed959a6c6f2bdfe
DrGBL <guillaume_bl@hotmail.com>
2021-02-09 22:06:51 -0500

    Update 02.hail.py

--- 02.hail.py
+++ 02.hail.py
@@ -3,15 +3,30 @@
 #specifically, variant recalibration should still be done, even if not shown here, can discuss with me on how to do it using gatk.
 
 import hail as hl
+import hail.expr.aggregators as agg
 
-#tmp_dir is where some of the temporary computations are done. I would make sure to assign it to a folder that does not have a strict data cap.
-hl.init(spark_conf=None, tmp_dir='/path/to/tmp_dir/')
+#the different paths below
+
+# this is where some of the temporary computations are done. I would make sure to assign it to a folder that does not have a strict data cap.
+pathTMP = '/scratch/richards/guillaume.butler-laporte/tmpSort/'
+hl.init(spark_conf=None, tmp_dir= pathTMP)
+
+# this is the path to your vcf from step 1
+pathNorm = '/scratch/richards/guillaume.butler-laporte/WGS/normalizedVCF/allSamples.normID.noChrM.vqsr.flt.vcf.gz'
+
+# this is the path of the matrix table hail will create
+pathMT = '/scratch/richards/guillaume.butler-laporte/WGS/hailFiles/hail.full.normID.noChrM.mt'
+
+# this is path to the QCed vcf file output. Important that the end of the file be ".vcf.bgz" otherwise hail will not provide correct format
+pathQC = '/scratch/richards/guillaume.butler-laporte/WGS/QCed/allSamples.normID.GTflt.AB.noChrM.vqsr.flt.vcf.bgz'
+
+
 
 #import the data and sample QC
-hl.import_vcf('/path/to/sequence.file.normID.noChrM.vcf.gz', min_partitions=4, reference_genome='GRCh38', force_bgz=True).write('/hailFiles/hail.full.normID.noChrM.mt', overwrite=True)
-metaData = hl.get_vcf_metadata('/path/to/sequence.file.normID.noChrM.vcf.gz')
+hl.import_vcf(pathNorm, min_partitions=4, reference_genome='GRCh38', force_bgz=True).write(pathMT, overwrite=True)
+metaData = hl.get_vcf_metadata(pathNorm)
 
-mtAll = hl.read_matrix_table('/hailFiles/hail.full.noChrM.mt')
+mtAll = hl.read_matrix_table(pathMT)
 mtAll= mtAll.annotate_entries(AB = (mtAll.AD[1] / hl.sum(mtAll.AD) ))
 mtAll=hl.sample_qc(mtAll)
 mtAll = mtAll.filter_cols((mtAll.sample_qc.call_rate >= 0.97) & (mtAll.sample_qc.dp_stats.mean >= 20))
@@ -21,4 +36,4 @@ mtAll = mtAll.filter_entries( (mtAll.GQ>=20) &
                         (mtAll.GT.is_het() & (mtAll.AB >= 0.25) & (mtAll.AB <= 0.75)) |
                         (mtAll.GT.is_hom_var() & (mtAll.AB >= 0.9))))
                         
-hl.export_vcf(mtAll, '/path/to/sequence.file.normID.GTflt.AB.noChrM.vcf.bgz', metadata=metaData, tabix=True)
+hl.export_vcf(mtAll, pathQC, metadata=metaData, tabix=True)
