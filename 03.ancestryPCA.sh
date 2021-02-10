02da9892f1a3fbbec9c082990e0d8fc7b523978d
DrGBL <guillaume_bl@hotmail.com>
2021-02-09 22:30:04 -0500

    Update 03.ancestryPCA.sh

--- 03.ancestryPCA.sh
+++ 03.ancestryPCA.sh
@@ -1,8 +1,7 @@
 #this may take some time to run in its entirety
 
-#the code is based on this https://www.biostars.org/p/335605/
-#it was further modified to run a random forest classifier
-#requires R, plink 1.9, and bcftools >1.3
+#based on this https://www.biostars.org/p/335605/
+#requires plink 1.9 and bcftools >1.3
 
 #the idea here is as follows:
 #1) obtain the 1000G reference files (in GRCh38 always, only SNPs, and autosomal chromosomes), normalize and left-align them with the reference genome
@@ -14,7 +13,21 @@
 #7) predict the ancestry in your cohort using that trained random forest and your cohort's projection on 1000G's PCs
 #8) output files named like "afrIDsPCA.txt", for the study IDs of individuals predicted to be of a certain ancestry (here african, for example).
 
-#first, download the GRCh38 1000G data (the bandwitdh was slow when I tried, so it can be long)
+
+# paths
+
+# path to folder where computations will be done and results saved
+pathWork=/scratch/richards/guillaume.butler-laporte/WGS/1000G.PCA/
+
+# path to QCed vcf file (e.g. from hail)
+pathQC=/scratch/richards/guillaume.butler-laporte/WGS/QCed/allSamples.normID.GTflt.AB.noChrM.vqsr.flt.vcf.bgz
+
+
+cd $pathWork
+mkdir -p cohortSample
+mkdir -p Pruned
+
+# download the GRCh38 1000G data, if not done already
 
 prefix="http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20181203_biallelic_SNV/ALL.chr"
 suffix=".shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf.gz"
@@ -26,7 +39,7 @@ done
 #now will need to download ancestry information from the 1000G
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20130606_sample_info/20130606_g1k.ped
 
-#download reference genome fasta file (GRCh38), if not done in the previous step
+#download reference genome fasta file (GRCh38)
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 gzip -d GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 
@@ -35,12 +48,11 @@ for chr in {1..22}; do
     bcftools norm -m-any --check-ref w -f GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
       ALL.chr"${chr}".shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf.gz | \
       bcftools annotate -x ID -I +'%CHROM:%POS:%REF:%ALT' | \
-        bcftools norm -Ob --rm-dup both | \
+        bcftools norm -Ob --rm-dup both \
           > ALL.chr"${chr}".shapeit2_integrated_v1a.GRCh38.20181129.phased.bcf ;
 
     bcftools index ALL.chr"${chr}".shapeit2_integrated_v1a.GRCh38.20181129.phased.bcf ;
 done
-
 
 #convert bcf to plink format
 for chr in {1..22}; do
@@ -56,17 +68,16 @@ done
 
 #Obtain the list of variants for ancestry inference PCA
 for chr in {1..22}; do
-  bcftools filter -r chr${chr} /path/to/sequence.file.normID.GTflt.AB.noChrM.vcf.bgz -Ou | \
-  bcftools query -f "%ID\n" > /path/to/variants.chr${chr}.txt;
+  bcftools view -r chr${chr} $pathQC | \
+  bcftools query -f "%ID\n" > /cohortSample/variants.chr${chr}.txt;
 done
 
-#prune for common variants in linkage equilibrium, and only keeping variants also in your cohort
-mkdir -p Pruned ;
 
+#prune for common variants in linkage equilibrium, and only keeping variants also in your cohort
 for chr in {1..22}; do
     plink --noweb \
       --bfile ALL.chr"${chr}".shapeit2_integrated_v1a.GRCh38.20181129.phased \
-      --extract /path/to/variants.chr{$chr}.txt \
+      --extract /cohortSample/variants.chr"${chr}".txt \
       --maf 0.10 --indep 50 5 1.5 \
       --out Pruned/ALL.chr"${chr}".shapeit2_integrated_v1a.GRCh38.20181129.phased ;
 
@@ -88,9 +99,7 @@ plink --merge-list ForMerge.list --out Merge ;
 #make plink file from the full all sample VCF from your cohort, only keeping the pruned variants, then merge with the 1000G plink files
 awk '{ print $2 }' Merge.bim > MergeVariants.txt
 
-mkdir -p cohortSample
-
-plink --vcf /path/to/sequence.file.normID.GTflt.AB.noChrM.vcf.bgz \
+plink --vcf $pathQC \
  --noweb \
  --extract MergeVariants.txt \
  --make-bed \
@@ -98,8 +107,8 @@ plink --vcf /path/to/sequence.file.normID.GTflt.AB.noChrM.vcf.bgz \
 
 printf "Merge\n./cohortSample/cohortSample" > ForMergeFull.list
 
-plink --merge-list ForMergeFull.list --out MergeFullForPCA ;
-
+plink --merge-list ForMergeFull.list --out MergeFullForPCA 
+ 
 #now divide between 1000G and cohort samples
 awk '{ print $1,$2 }' Merge.fam | awk '$(NF+1) = "1000G"' > 1000G.cluster.txt
 awk '{ print $1,$2 }' cohortSample/cohortSample.fam | awk '$(NF+1) = "Cohort"' > cohort.cluster.txt
@@ -110,92 +119,3 @@ plink --bfile MergeFullForPCA \
  --pca-cluster-names 1000G \
  --pca \
  --within clusters.txt
-
-
-#the following happens in R  
-R
-
-#will use randomForest for the classifier. Need to install the package below if not done.
-
-library(randomForest)
-
-# read in the eigenvectors, produced in PLINK
-eigenvec <- read.table('plink.eigenvec', header = FALSE, skip=0, sep = ' ')
-eigenvec <- eigenvec[,2:ncol(eigenvec)]
-colnames(eigenvec) <- c("Individual.ID",paste('PC', c(1:20), sep = ''))
-
-# read in the PED data
-PED <- read.table('20130606_g1k.ped', header = TRUE, skip = 0, sep = '\t')
-
-#build data frame for random forest classifier
-dataRF <- merge(eigenvec, PED[, c("Individual.ID", "Population")], all.x=TRUE)
-
-#build plot
-dataRF$Population <- factor(dataRF$Population, levels=c(
-  "ACB","ASW","ESN","GWD","LWK","MSL","YRI",
-  "CLM","MXL","PEL","PUR",
-  "CDX","CHB","CHS","JPT","KHV",
-  "CEU","FIN","GBR","IBS","TSI",
-  "BEB","GIH","ITU","PJL","STU"))
-
-dataRF$Continental <- rep(NA_character_, nrow(dataRF))
-dataRF$Continental[which(dataRF$Population %in% c("ACB","ASW","ESN","GWD","LWK","MSL","YRI"))]<-"AFR"
-dataRF$Continental[which(dataRF$Population %in% c("CLM","MXL","PEL","PUR"))]<-"AMR"
-dataRF$Continental[which(dataRF$Population %in% c("CDX","CHB","CHS","JPT","KHV"))]<-"EAS"
-dataRF$Continental[which(dataRF$Population %in% c("CEU","FIN","GBR","IBS","TSI"))]<-"EUR"
-dataRF$Continental[which(dataRF$Population %in% c("BEB","GIH","ITU","PJL","STU"))]<-"SAS"
-dataRF$Continental<-as.factor(dataRF$Continental)
-
-
-col <- colorRampPalette(c(
-  "yellow","forestgreen","grey","royalblue","black"))(length(unique(dataRF$Continental)))[factor(dataRF$Continental)]
-
-
-#PCA plots, to see if you get ok results, you should see clusters.
-par(mar = c(5,5,5,5), cex = 2.0,
-    cex.main = 7, cex.axis = 2.75, cex.lab = 2.75, mfrow = c(1,2))
-
-plot(dataRF[,2], dataRF[,3],
-     type = 'n',
-     main = 'A',
-     adj = 0.5,
-     xlab = 'First component',
-     ylab = 'Second component',
-     font = 2,
-     font.lab = 2)
-points(dataRF[,2], dataRF[,3], col = col, pch = 20, cex = 2.25)
-legend('bottomright',
-       bty = 'n',
-       cex = 3.0,
-       title = '',
-       c('Population 1', 'Population 2', 'Population 3',
-         'Population 4', 'Population 5'),
-       fill = c('yellow', 'forestgreen', 'grey', 'royalblue', 'black'))
-
-plot(dataRF[,2], dataRF[,4],
-     type="n",
-     main="B",
-     adj=0.5,
-     xlab="First component",
-     ylab="Third component",
-     font=2,
-     font.lab=2)
-points(dataRF[,2], dataRF[,4], col=col, pch=20, cex=2.25)
-
-
-#random forest
-rf_classifier = randomForest(Continental ~ ., data=dataRF[which(!is.na(dataRF$Continental)),c("PC1","PC2","PC3","PC4","PC5","PC6", "Continental")], ntree=10000, importance=TRUE)
-
-#predict ancestries
-dataPred<-dataRF[which(is.na(dataRF$Continental)),c("Individual.ID", "PC1", "PC2", "PC3","PC4","PC5","PC6")]
-dataPred$Prediction<-rep(NA, nrow(dataPred))
-dataPred$Prediction<-predict(rf_classifier,dataPred[,c("PC1","PC2","PC3","PC4","PC5","PC6")])
-
-#write IDs per ancestry
-write(as.character(subset(dataPred, Prediction=="AFR")$Individual.ID), "afrIDsPCA.txt")
-write(as.character(subset(dataPred, Prediction=="AMR")$Individual.ID), "amrIDsPCA.txt")
-write(as.character(subset(dataPred, Prediction=="EAS")$Individual.ID), "easIDsPCA.txt")
-write(as.character(subset(dataPred, Prediction=="EUR")$Individual.ID), "eurIDsPCA.txt")
-write(as.character(subset(dataPred, Prediction=="SAS")$Individual.ID), "sasIDsPCA.txt")
-
-q()
