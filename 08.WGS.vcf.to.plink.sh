f745c294dc6f1bcd569ce3f9e76fcba1220a74a7
DrGBL <guillaume_bl@hotmail.com>
2021-02-09 23:27:10 -0500

    Update 08.WGS.vcf.to.plink.sh

--- 08.WGS.vcf.to.plink.sh
+++ 08.WGS.vcf.to.plink.sh
@@ -1,27 +1,28 @@
-#this takes the filtered vcf file from the WGS sequencing, removes variants with MAF < 1%, MAC < 5, and for whom HWE is not reached, and outputs the plink binary file.
-#genotype call rate and MAF threshold is set high to avoid quasi-separation, since step 1 does not use Firth
-#this file is to be used in step 1 of regenie. regenie does not accept vcf as inputs.
-#warning: may take a bit of time.
+#this function does two things necessary for regenie step 1 and 2 outputs
+# 1) it outputs the QCed vcf restricted to the chosen ancestry to a plink binary file (used in step 1 and step 2)
+# 2) from this plink binary file, it further filters variants to retain only those in linkage equilibrium with MAF>1% and that attained HWE (used in step 1 only)
+# the filtering of variants above is used to build the polygenic score in regenie's step 1
 
-mkdir -p regenieInputs
+#paths
 
-plink --vcf /path/to/sequence.file.Eur.normID.GTflt.AB.noChrM.vcf.gz \
- --hwe 1E-6 midp \
- --make-bed \
+#path to given ancestry QCed vcf
+pathAncestry=/scratch/richards/guillaume.butler-laporte/WGS/allSamples.Eur.normID.GTflt.AB.noChrM.vqsr.flt.vcf.gz
+
+#path to given ancestry QCed plink binary
+pathPlink=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/allSamples.Eur.normID.GTflt.AB.noChrM.vqsr.flt
+
+#path to pruned variants
+pathPruned=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/allSamples.regenie.LD.prune.maf0.01.geno0.1.Eur.normID.GTflt.AB.noChrM.vqsr.flt
+
+
+plink --vcf $pathAncestry  \
+  --make-bed \
+  --out $pathPlink
+
+# LD pruned variants for regenie step 1
+plink --bfile $pathPlink \
+ --hwe 1E-15 midp \
  --maf 0.01 \
- --mac 5 \
- --geno 0.1 \
- --out /regenieInputs/sequence.file.Eur.normID.GTflt.AB.noChrM 
-
-
-#suggest to also prune for LD, to speed up regenie step 1, if this is done, then the following code should be ran (instead of the plink call above).
-
-plink --vcf /path/to/sequence.file.Eur.normID.GTflt.AB.noChrM.vcf.gz \
- --hwe 1E-6 midp \
- --make-bed \
- --maf 0.01 \
- --mac 5 \
  --geno 0.1 \
  --indep-pairwise 50 5 0.05 \
- --out /regenieInputs/sequence.file.Eur.normID.GTflt.AB.noChrM 
- 
+ --out $pathPruned
