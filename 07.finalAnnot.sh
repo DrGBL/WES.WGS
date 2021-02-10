46aa8bfb98afca9284bb3c2c89c5211a5beb1365
DrGBL <guillaume_bl@hotmail.com>
2021-02-09 23:20:22 -0500

    Update 07.finalAnnot.sh

--- 07.finalAnnot.sh
+++ 07.finalAnnot.sh
@@ -4,16 +4,25 @@
 #the --fork 10 makes it faster, but can give trouble sometimes
 #here I did it on the european population, but it can be done once on the whole sample (then used in each ancestry stratified analysis).
 
-mkdir -p annotation
+#paths
+#path to output folder
+pathAnnot=/scratch/richards/guillaume.butler-laporte/WGS/annotation/
 
-for chr in {1..22} X; do
-  vep -i /path/to/sequence.file.chr"${chr}".Eur.normID.GTflt.AB.noChrM.vcf.gz \
+#path to VEP cache directory
+pathCache=/project/richards/guillaume.butler-laporte/bin/.vep
+
+for x in {1..22} X; do
+  #set the path to splitchromosomes
+  pathSplit=/scratch/richards/guillaume.butler-laporte/WGS/splitChrom/allSamples.chr"${x}".Eur.normID.GTflt.AB.noChrM.vqsr.flt.vcf.gz
+
+  vep -i $pathSplit \
     --plugin dbNSFP,/project/richards/guillaume.butler-laporte/bin/dbSNFP4.0a_files/dbNSFP4.0a.gz,Ensembl_transcriptid,Uniprot_acc,VEP_canonical,LRT_pred,SIFT_pred,MutationTaster_pred,Polyphen2_HDIV_pred,Polyphen2_HVAR_pred \
     -everything \
-    --buffer_size 10000 \
-    --fork 10 \
+    --buffer_size 100000 \
+    --force_overwrite \
     --offline \
-    --dir_cache /path/to/cache/directory \
-    --cache -o /annotation/finalAnnot.chr"${chr}".txt ;
+    --fork 10 \
+    --dir_cache $pathCache \
+    --cache -o "${pathAnnot}"finalAnnot.chr${x}.txt;
 done
 
