3ae2ef64a5c854d678b2edbdf72bed83ba59617a
DrGBL <guillaume_bl@hotmail.com>
2021-02-09 23:34:21 -0500

    Create 09.variantPrep.sh

---
+++ 09.variantPrep.sh
@@ -0,0 +1,12 @@
+#this file may be difficult to understand, but essentially it just builds the --set-list and --anno-file (regenie step 2 inputs) from the VEP annotation output
+#if PBS arrays are available on your cluster, I highly suggest you to do it this way (instead of the for loop below).
+
+
+
+
+for x in {1..22} X; do
+
+
+
+
+done
