87e316f7fc00bf20aedfac6ff58b6ed8f0c35892
DrGBL <guillaume_bl@hotmail.com>
2021-06-01 14:44:54 -0400

    Update 02.hail.py

--- 02.hail.py
+++ 02.hail.py
@@ -4,6 +4,7 @@
 
 import hail as hl
 import hail.expr.aggregators as agg
+from bokeh.io import export_png #this requires selenium (conda install -c bokeh selenium) and phantomjs (conda install phantomjs)
 
 #the different paths below
 
@@ -27,7 +28,9 @@ hl.import_vcf(pathNorm, min_partitions=4, reference_genome='GRCh38', force_bgz=T
 metaData = hl.get_vcf_metadata(pathNorm)
 
 mtAll = hl.read_matrix_table(pathMT)
+mtAll.count()
 mtAll= mtAll.annotate_entries(AB = (mtAll.AD[1] / hl.sum(mtAll.AD) ))
+mtAll=hl.variant_qc(mtAll)
 mtAll=hl.sample_qc(mtAll)
 mtAll = mtAll.filter_cols((mtAll.sample_qc.call_rate >= 0.97) & (mtAll.sample_qc.dp_stats.mean >= 20))
 mtAll = mtAll.filter_entries( (mtAll.GQ>=20) &
@@ -35,5 +38,16 @@ mtAll = mtAll.filter_entries( (mtAll.GQ>=20) &
                  ((mtAll.GT.is_hom_ref() & (mtAll.AB <= 0.1)) |
                         (mtAll.GT.is_het() & (mtAll.AB >= 0.25) & (mtAll.AB <= 0.75)) |
                         (mtAll.GT.is_hom_var() & (mtAll.AB >= 0.9))))
-                        
+mtAll = mtAll.filter_rows((mtAll.variant_qc.gq_stats.mean > 10) & (mtAll.variant_qc.dp_stats.mean > 5) & (mtAll.variant_qc.call_rate > 0.8))
+mtAll.count()
+             
+#plot different QC metrics
+p_sample_call=hl.plot.histogram(mtAll.sample_qc.call_rate,legend='Sample Call Rate')
+p_variant_call=hl.plot.histogram(mtAll.variant_qc.call_rate,legend='Variant Call Rate')
+p_mean_variant_dp=hl.plot.histogram(mtAll.variant_qc.dp_stats.mean,legend='Sample Mean DP')
+export_png(p_sample_call, filename="/scratch/richards/guillaume.butler-laporte/tmpSort/p_sample_call.png")
+export_png(p_variant_call, filename="/scratch/richards/guillaume.butler-laporte/tmpSort/p_variant_call.png")
+export_png(p_mean_variant_dp, filename="/scratch/richards/guillaume.butler-laporte/tmpSort/p_mean_variant_dp.png")
+ 
+#export back to vcf          
 hl.export_vcf(mtAll, pathQC, metadata=metaData, tabix=True)
