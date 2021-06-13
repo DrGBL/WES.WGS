65874b584d1f5c922d16543ddfdb2e5a190b7947
DrGBL <guillaume_bl@hotmail.com>
2021-06-13 14:06:18 -0400

    Update 08.WGS.vcf.to.plink.sh

--- 08.WGS.vcf.to.plink.sh
+++ 08.WGS.vcf.to.plink.sh
@@ -1,20 +1,12 @@
 #this function does two things necessary for regenie step 1 and 2 outputs
-# 1) it outputs the QCed vcf restricted to the chosen ancestry to a plink binary file (used in step 1 and step 2)
-# 2) it assigns a female sex to all samples, in order for the chromosome X to be coded as 0/2 for males. This is just a trick to make plink readjust the phenotypes
-# and sex is added separately as a covariate in the analysis (step 11).
-# 3) from this plink binary file, it further filters variants to retain only those in linkage equilibrium with MAF>1% and that attained HWE (used in step 1 only)
+# 1) it outputs the QCed vcf restricted to the chosen ancestry to a plink binary file (used in regenie step 2)
+# 2) from this plink binary file, it further filters variants to retain only those in linkage equilibrium with MAF>1% and that attained HWE (used in step 1 only)
 # the filtering of variants above is used to build the polygenic score in regenie's step 1.
 
 #paths
 
 #path to given ancestry QCed vcf
 pathAncestry=/scratch/richards/guillaume.butler-laporte/WGS/allSamples.swedes.exome.Eur.normID.GTflt.AB.noChrM.vqsr.flt.vcf.gz
-
-#path to chrX adjusted plink
-pathPlinkTmp=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/allSamples.swedes.exome.Eur.normID.GTflt.AB.noChrM.vqsr.flt.tmp
-
-#path to FID IID and new sex (all female) file (input for plink)
-allFemale=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/all_female.txt
 
 #path to given ancestry QCed plink binary
 pathPlink=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/allSamples.swedes.exome.Eur.normID.GTflt.AB.noChrM.vqsr.flt
@@ -24,13 +16,6 @@ pathPruned=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/allSampl
 
 
 plink --vcf $pathAncestry  \
-  --make-bed \
-  --out $pathPlinkTmp
-
-awk '{print $1, $2, "2"}' $pathPlinkTmp.fam > $allFemale  
-
-plink --bfile $pathPlinkTmp \
-  --update-sex $allFemale \
   --make-bed \
   --out $pathPlink
 
