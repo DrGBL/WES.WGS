d0c7f5224da745c1437f757cdd221f8b5d70b4d9
DrGBL <guillaume_bl@hotmail.com>
2021-10-25 12:28:52 -0400

    Update 12.regenieAnalysis_single_variants.sh

--- 12.regenieAnalysis_single_variants.sh
+++ 12.regenieAnalysis_single_variants.sh
@@ -1,1 +1,49 @@
+#paths
 
+#path to QCed plink binaries
+pathPlink=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/allSamples.Eur.normID.GTflt.AB.noChrM.vqsr.flt
+#path to covariates file
+pathCov=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/covar.txt
+#path to phenotype file
+pathPheno=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/pheno.txt
+#path to regenie tmp folder
+pathTmpReg=/scratch/richards/guillaume.butler-laporte/WGS/regenieRes/tmp/
+#path to pruned variants
+pathPruned=/scratch/richards/guillaume.butler-laporte/WGS/regenieInputs/allSamples.regenie.LD.prune.maf0.01.geno0.1.Eur.normID.GTflt.AB.noChrM.vqsr.flt
+#path to regenie results folder
+pathOut=/scratch/richards/guillaume.butler-laporte/WGS/regenieRes/
+
+#name of your cohort
+name=name.of.your.cohort
+
+
+#step 1 can be the same as the one from the burden test (see step 11), but just in case, I've put it here too.
+
+regenie \
+  --step 1 \
+  --bed $pathPlink \
+  --covarFile $pathCov \
+  --phenoFile $pathPheno \
+  --bt \
+  --lowmem \
+  --lowmem-prefix "${pathTmpReg}"tmp_rg \
+  --extract "${pathPruned}".prune.in \
+  --bsize 1000 \
+  --out "${pathOut}"BQC19.hc.080921.eur.step1.single_variant
+
+regenie \
+  --step 2 \
+  --minMAC 6 \
+  --covarFile $pathCov \
+  --phenoFile $pathPheno \
+  --bed ${pathPlink} \
+  --bt \
+  --htp $name \
+  --firth --approx \
+  --firth-se \
+  --maxstep-null 5 \
+  --maxiter-null 10000 \
+  --pred "${pathOut}"BQC19.hc.080921.eur.step1.single_variant_pred.list \
+  --bsize 200 \
+  --out "${pathOut}"BQC19.hc.080921.eur.step2.single_variant
+  
