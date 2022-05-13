ac19a9c928411380c6277ec8cca6c8c2edb0f783
DrGBL <guillaume_bl@hotmail.com>
2022-05-13 19:53:30 -0400

    Update 02.pca_projection.sh

--- PCA_PROJECTION/02.pca_projection.sh
+++ PCA_PROJECTION/02.pca_projection.sh
@@ -1,6 +1,6 @@
 sscore_file=/path/to/sscore/file #the output from the previous step
 pheno_file=/path/to/phenotype/file #the same phenotype file you provided as input to regenie for the association analyses
-pheno_col="string" #A, B, or C, or whatever else you called them
+pheno_col="string" #A, B, or C, or whatever else you called them, note that individuals without a phenotype are automatically removed, which should make your life easier in situations where not all ancestries were used for all analyses.
 study_name="string" #your study name
 ref_score=/path/to/1000G_snps_scores.txt.gz
 path_out=/path/out/folder/ #only the folder, the name of the files are auto-filled
@@ -20,3 +20,16 @@ Rscript plot_projected_pc.R \
 
 
 #if multiple ancestries
+path_anc_file=/path/to/anc/file # this file has at least three column: FID, IID, and the ancestry column
+anc_column="string" #ancestry column name
+
+Rscript plot_projected_pc.R \
+  --sscore ${sscore_file} \
+  --phenotype-file ${pheno_file} \
+  --phenotype-col ${pheno_col} \
+  --ancestry-file ${path_anc_file} \
+  --ancestry-col ${anc_column} \
+  --plot-pc-num 10 \
+  --study ${study_name} \
+  --reference-score-file ${ref_score} \
+  --out ${path_out}/${study_name}_${pheno_col}_projection
